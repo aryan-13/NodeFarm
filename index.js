b47f9ee964f184e020ca0aec978e25b8e2294668
Aryan <aryanchaudhary13@gmail.com>
2020-10-19 10:25:03 +0530

    Functionality added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
-
+const replaceTemplate = require('./modules/replaceTemplate');
 ////////////////////////////////////
 //////FILES
 // Blocking
@@ -29,20 +29,40 @@ const url = require('url');
 
 ////////////////////////////////////
 //////SERVER
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
+    const { query, pathname } = url.parse(req.url, true);
 
-    if (pathName === '/' || pathName === '/overview') {
-        res.end('This is an overview!');
-    } else if (pathName === '/product') {
-        res.end('This is an PRODUCT!');
-    } else if (pathName === '/api') {
-        res.writeHead(200, { 'Conten-type': 'application/json' });
+    //OVERVIEW PAGE
+    if (pathname === '/' || pathname === '/overview') {
+        res.writeHead(200, { 'Content-type': 'text/html' });
+
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+        res.end(output);
+
+        //PRODUCT PAGE
+    } else if (pathname === '/product') {
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product)
+        res.end(output);
+
+        //API
+    } else if (pathname === '/api') {
+        res.writeHead(200, { 'Content-type': 'application/json' });
         res.end(data);
+
+        // NOT FOUND
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html',
@@ -53,5 +73,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8000, '127.0.0.1', () => {
-    console.log('Listening to req1uests on port 8000!');
+    console.log('Listening to requests on port 8000!');
 });
